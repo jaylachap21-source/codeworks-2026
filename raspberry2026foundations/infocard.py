2447ef5812c3b896791d0e73741f9701a46c4cc0
jaylachap21-source <jaylachap21@gmail.com>
2026-06-30 14:18:46 -0400

    added info

---
+++ raspberry2026foundations/infocard.py
@@ -0,0 +1,12 @@
+name = "jayla"
+age = "14"
+interest = "music"
+why_here = "money"
+goals = "understand how to code"
+personality = "nice
+print ("name:", name)
+print ("age:", age)
+print ("interest:", interest)
+print ("goals:", goals)
+print ("why_here:", why_here)
+print ("personality:", personality)
